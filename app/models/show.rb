baeb3897bfc7b35d39fdafbbf6db1ab95affba13
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-28 00:46:36 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -19,9 +19,11 @@ def self.least_popular_show
 end
 # ratings_sum: returns the sum of all of the ratings.
 def self.ratings_sum
-  
+
 end
 # popular_shows: returns an array of all of the shows that have a rating greater than 5. hint: use the where Active Record method.
-#
+def self.popular_shows
+
+end
 # shows_by_alphabetical_order: returns an array of all of the shows sorted by alphabetical order according to their names. hint: use the order Active Record method.
 end
